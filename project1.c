3ffc3df39491d2e6e5048ad9015b2ecc1724ec55
osboxes.org <osboxes@osboxes.org>
2020-01-24 17:26:11 -0500

    Linked list of processes createdgit add .

--- project1.c
+++ project1.c
@@ -10,8 +10,31 @@
 //we have 1 PID, we can get 4  PPID and 23 VSIZE and 2 COMM from fopen and fscanthen we can cross reference every PID against every other process's PPID and when there is a match the PID needs to link to the child process. Then some sort ofQUEUE to help with the tabbing of the output to console.
 
 
-int main(int argc, char **args)
-{	int rv;
+// Structure of a process
+struct process{
+	int pid;
+	int ppid;
+	char *comm;
+	unsigned long int vsize;
+	int level;
+	struct process *next;
+};
+
+int main(int argc, char **args){
+
+	// prev will save the process to set the next
+	struct process *prev = NULL;
+	// starting process of zero (known as the swapper process, will be the highest level "process")
+	struct process *zero = malloc(sizeof(struct process));
+	zero->pid = 0;
+	zero->ppid = -1;
+	zero->comm = "zero";
+	zero->vsize = 0;
+	zero->level = 0;
+	zero->next = NULL;
+	prev = zero;
+
+	int rv;
 	regmatch_t match[1000];
 	regex_t exp;
 	rv = regcomp(&exp, "^[0-9]", REG_EXTENDED);
@@ -35,34 +58,41 @@ int main(int argc, char **args)
 	    rv = regexec(&exp, de->d_name, 1000, match, REG_NOTEOL);
 	   // printf("%d", rv);
 	    if( rv == 0)   {
-		char comm[250] = "cat";
-		char state;
-		int pid, ppid, pgrrp, session, tty_nr, tpgid;
-		unsigned int flags;
-		unsigned long int minflt, cminflt, majflt, cmajflt, utime, stime, vsize;
-		long int cutime, cstime, priority, nice, num_threads, itrealvalue;
-		unsigned long long int starttime;
-		char *procnum = malloc(12 + strlen(de->d_name));
-		//procnum = strcat("/proc/", de->d_name);
-		//procnum = strcat(procnum, "/stat");
+			char comm[250] = "cat";
+			char state;
+			int pid, ppid, pgrrp, session, tty_nr, tpgid;
+			unsigned int flags;
+			unsigned long int minflt, cminflt, majflt, cmajflt, utime, stime, vsize;
+			long int cutime, cstime, priority, nice, num_threads, itrealvalue;
+			unsigned long long int starttime;
+			char *procnum = malloc(12 + strlen(de->d_name));
+			//procnum = strcat("/proc/", de->d_name);
+			//procnum = strcat(procnum, "/stat");
 
-		strcpy( procnum, "/proc/");
-		strcat( procnum, de->d_name);
-		strcat( procnum, "/stat");
-	    	fp = fopen(procnum, "r");
-		   // printf("%s\n", de->d_name);
-		fscanf(fp, "%d %s %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %ld %ld %ld %ld %llu %lu",
-						&pid, comm, &state, &ppid, &pgrrp, &session, &tty_nr, &tpgid, &flags,
-						&minflt, &cminflt, &majflt, &cmajflt, &utime, &stime, &cutime, &cstime, &priority, &nice, &num_threads,
-						&itrealvalue, &starttime, &vsize);
-	    		printf(" this is the pid %d, this is the comm %s\n", pid, comm);
-					printf("this is the state %c and ppid %d\n", state, ppid);
-					printf("%d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %ld %ld %ld %ld %llu\n%lu\n", pgrrp, session, tty_nr, tpgid, flags,
-									minflt, cminflt, majflt, cmajflt, utime, stime, cutime, cstime, priority,
-									nice, num_threads, itrealvalue, starttime, vsize);
-					/*printf(" this is the ppid %d, this is the memsize %lu bytes", ppid, vsize);*/
+			strcpy( procnum, "/proc/");
+			strcat( procnum, de->d_name);
+			strcat( procnum, "/stat");
+		    	fp = fopen(procnum, "r");
+			   // printf("%s\n", de->d_name);
+			// Defining how to read the stat file, up to the memory size(vsize)
+			fscanf(fp, "%d %s %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %ld %ld %ld %ld %llu %lu",
+							&pid, comm, &state, &ppid, &pgrrp, &session, &tty_nr, &tpgid, &flags,
+							&minflt, &cminflt, &majflt, &cmajflt, &utime, &stime, &cutime, &cstime, &priority, &nice, &num_threads,
+							&itrealvalue, &starttime, &vsize);
+				// Allocate space and set information from file
+				struct process *new_proc = malloc(sizeof(struct process));
+				// Linke previous to the new structure
+				prev->next = new_proc;
+				new_proc->pid = pid;
+				new_proc->ppid = ppid;
+				new_proc->comm = comm;
+				new_proc->vsize = vsize;
+				new_proc->level = 0;
+				new_proc->next = NULL;
+				prev = new_proc;
 	    }
   	}
+
     closedir(dr);
     return 0;
 }
