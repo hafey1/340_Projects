2fd489095271397c74b1d3c3385e4ade296af4da
hafey1 <r.hafen@yahoo.com>
2020-01-25 13:46:52 -0500

    Some final-ish changes, used a function to free the memory and commented code along with adding some reference points used to create this program.

--- project1.c
+++ project1.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
-//#include </usr/include/linux/sched.h>
 #include <regex.h>
 #include <string.h>
 
-// this code is borrowed from geeksforgeeks.org/c-program-list-files-sub-directories-directory/
-
-//we have 1 PID, we can get 4  PPID and 23 VSIZE and 2 COMM from fopen and fscanthen we can cross reference every PID against every other process's PPID and when there is a match the PID needs to link to the child process. Then some sort ofQUEUE to help with the tabbing of the output to console.
-
-
-// Structure of a process
+// the base of this code is can be found from geeksforgeeks.org/c-program-list-files-sub-directories-directory/
+// and in the struct.c file provided along with this file in the repo
 struct process{
 	int pid;
 	int ppid;
@@ -20,6 +15,18 @@ struct process{
 	struct process *next;
 };
 
+//because we are in C we must free the memory after we are done using it
+void freeTheLinkedList(struct process *start) {
+	
+	struct process *beFree;
+
+	while ((beFree = start) != NULL) {
+	start = start->next;
+	free(beFree);
+	}
+}
+
+
 // this is a recursive function that takes a pointer to the beginning of the linked list of processes, and the pointer to the established parent process
 void printOutFileSystem(struct process *start, struct process *parent) {
 
@@ -27,7 +34,9 @@ void printOutFileSystem(struct process *start, struct process *parent) {
 	//the parent cross reference is passed in from the recursive function call
 	struct process *childFinder = start;
 	struct process *parentChecker = parent;
-
+	
+	//this piece allows us to adjust spaces to be printed out according to the level of the directory
+	//which is always 1 more than the parents
 	char spaces[250]= "";
 	for (int i = 0; i < parentChecker->level; i++) {
 		strcat(spaces, "  ");
@@ -55,8 +64,6 @@ void printOutFileSystem(struct process *start, struct process *parent) {
 
 
 
-
-
 int main(int argc, char **args){
 
 	// prev will save the process to set the next
@@ -71,12 +78,12 @@ int main(int argc, char **args){
 	zero->next = NULL;
 	prev = zero;
 
+	//we use regex to capture processes that have the first character in the name as a digit value
 	int rv;
 	regmatch_t match[1000];
 	regex_t exp;
 	rv = regcomp(&exp, "^[0-9]", REG_EXTENDED);
 	FILE *fp;
-	//struct pid *superpoint = task_pid(1);
        	struct dirent *de;  // Pointer for directory entry
 
     // opendir() returns a pointer of DIR type.
@@ -91,27 +98,29 @@ int main(int argc, char **args){
     // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
     // for readdir()
     while ((de = readdir(dr)) != NULL){
-	//printf("%s\n", de->d_name);
+		//this takes the pointer to the compiled regular expression and runs it through the current directory for a match
+		//luckily if it is a match it returns a 0
 	    rv = regexec(&exp, de->d_name, 1000, match, REG_NOTEOL);
-	   // printf("%d", rv);
+
 	    if( rv == 0)   {
-			char *comm;
+		    	//the black magic of importing every piece of information necessary to get to VSize :)
+			//the string must be allocated in order to be assigned properly to each individual structure
+		    	char *comm;
 			comm = malloc(sizeof(char)*30);
+			
 			char state;
 			int pid, ppid, pgrrp, session, tty_nr, tpgid;
 			unsigned int flags;
 			unsigned long int minflt, cminflt, majflt, cmajflt, utime, stime, vsize;
 			long int cutime, cstime, priority, nice, num_threads, itrealvalue;
 			unsigned long long int starttime;
-			char *procnum = malloc(12 + strlen(de->d_name));
-			//procnum = strcat("/proc/", de->d_name);
-			//procnum = strcat(procnum, "/stat");
 
+			char *procnum = malloc(12 + strlen(de->d_name));
 			strcpy( procnum, "/proc/");
 			strcat( procnum, de->d_name);
 			strcat( procnum, "/stat");
 		    	fp = fopen(procnum, "r");
-			   // printf("%s\n", de->d_name);
+
 			// Defining how to read the stat file, up to the memory size(vsize)
 			fscanf(fp, "%d %s %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %ld %ld %ld %ld %llu %lu",
 							&pid, comm, &state, &ppid, &pgrrp, &session, &tty_nr, &tpgid, &flags,
@@ -128,7 +137,6 @@ int main(int argc, char **args){
 				new_proc->level = 0;
 				new_proc->next = NULL;
 				prev = new_proc;
-				//printf("%s\n", new_proc->comm);
 	    }
   	}
 
@@ -136,16 +144,19 @@ int main(int argc, char **args){
 	printOutFileSystem(zero, cats);
 
   closedir(dr);
-
+	
+  	//two different ways of freeing the structures
 	//clear that data
-	struct process *clearPointer;
+	/*struct process *clearPointer;
 	struct process *otherPoint;
 	clearPointer = zero;
 	while(clearPointer != NULL){
 		otherPoint = clearPointer;
 		clearPointer = clearPointer->next;
 		free(otherPoint);
-	}
+	}*/
+	
+	freeTheLinkedList(zero);
 
     return 0;
 }
