b38c7340a399775bc29e333083073ab50efec076
Isabel Lally <lallyir@g.cofc.edu>
2020-02-29 20:47:37 -0500

    Sorry, I'm adding not needed files

--- PagingSim/pagingSim.py
+++ PagingSim/pagingSim.py
@@ -18,29 +18,45 @@ def checkSeed(randSeed):
 
 def validPageSize(memSize, pageSize):
     # A method to check that the memory size is an even multiple of page size
-    if (int(memSize)%int(pageSize)) != 0:
+    if (memSize%pageSize) != 0:
         print("Page Size Must Be An Even Multiple Of Memory Size")
         sys.exit(1)
+
+def jobCreations(runTimeMin, runTimeMax, memMin, memMax, randSeed, numJobs):
+    # A method to create jobs and print them
+    runTime = []
+    jobMem = []
+    print("Job Queue:\n  Job #    Runtime    Memory")
+    for job in range(numJobs):
+        random.seed(randSeed)
+        runTime.append(random.randint(runTimeMin, runTimeMax))
+        jobMem.append(random.randint(memMin, memMax))
+        print("{0: >7}{1: >11}{2: >10}".format((job+1), runTime[job], jobMem[job]))
+        randSeed+=1
+    return runTime, jobMem
+
+    return runTime, jobMem
 
 def main():
 
     # Get arguments from command line
     validInput(sys.argv)
-    memSize = sys.argv[1]
-    pageSize = sys.argv[2]
+    memSize = int(sys.argv[1])
+    pageSize = int(sys.argv[2])
     validPageSize(memSize, pageSize)
     # 'export RANDOM_SEED=decimal' in command line
-    randSeed = os.environ.get("RANDOM_SEED")
-    randSeed = checkSeed(randSeed)
-    numJobs = sys.argv[3]
-    runTimeMin = sys.argv[4]
-    runTimeMax = sys.argv[5]
-    memMin = sys.argv[6]
-    memMax = sys.argv[7]
+    randSeed = int(os.environ.get("RANDOM_SEED"))
+    randSeed = int(checkSeed(randSeed))
+    numJobs = int(sys.argv[3])
+    runTimeMin = int(sys.argv[4])
+    runTimeMax = int(sys.argv[5])
+    memMin = int(sys.argv[6])
+    memMax = int(sys.argv[7])
 
     print("Simulator Parameters:\n  Memory Size: {0}\n  Page Size: {1}\n  Random Seed: {2}\n"
           "  Number of Jobs: {3}\n  Runtime (min-max) Timesteps: {4}-{5}\n"
           "  Memory (min-max): {6}-{7}\n".format(memSize, pageSize, randSeed, numJobs,
                                                      runTimeMin, runTimeMax, memMin, memMax))
 
+    jobCreations(runTimeMin, runTimeMax, memMin, memMax, randSeed, numJobs)
 main()
