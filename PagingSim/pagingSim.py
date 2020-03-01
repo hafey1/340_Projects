d270c195e6e41106af598bda324130b486c22eab
Isabel Lally <lallyir@g.cofc.edu>
2020-02-29 19:44:41 -0500

    Paging Sim Started

---
+++ PagingSim/pagingSim.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import random
+
+def validInput(args):
+    # A method to check that the proper number of variables were given
+    if len(args) != 8:
+        print("Incorrect Input:")
+        print("python3 pagingSim.py <memory size> <page size> <number of jobs> <runtime min> "
+              "<runtime max> <memory min> <memory max>")
+        sys.exit(1)
+
+def checkSeed(randSeed):
+    # A method to check that the random seed has been set and chooses random if not
+    if randSeed == 'None'or randSeed == "":
+        randSeed = random.randint(1,100)
+    return randSeed
+
+def validPageSize(memSize, pageSize):
+    # A method to check that the memory size is an even multiple of page size
+    if (int(memSize)%int(pageSize)) != 0:
+        print("Page Size Must Be An Even Multiple Of Memory Size")
+        sys.exit(1)
+
+def main():
+
+    # Get arguments from command line
+    validInput(sys.argv)
+    memSize = sys.argv[1]
+    pageSize = sys.argv[2]
+    validPageSize(memSize, pageSize)
+    # 'export RANDOM_SEED=decimal' in command line
+    randSeed = os.environ.get("RANDOM_SEED")
+    randSeed = checkSeed(randSeed)
+    numJobs = sys.argv[3]
+    runTimeMin = sys.argv[4]
+    runTimeMax = sys.argv[5]
+    memMin = sys.argv[6]
+    memMax = sys.argv[7]
+
+    print("Simulator Parameters:\n  Memory Size: {0}\n  Page Size: {1}\n  Random Seed: {2}\n"
+          "  Number of Jobs: {3}\n  Runtime (min-max) Timesteps: {4}-{5}\n"
+          "  Memory (min-max): {6}-{7}\n".format(memSize, pageSize, randSeed, numJobs,
+                                                     runTimeMin, runTimeMax, memMin, memMax))
+
+main()
