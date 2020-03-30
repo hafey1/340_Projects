89eea20027a9242036d2c0b762c08ea14f44f94e
hafey1 <r.hafen@yahoo.com>
2020-03-30 17:52:41 -0400

    it brokededed

--- Proj3/proj3.c
+++ Proj3/proj3.c
@@ -25,27 +25,37 @@ typedef struct {
 } ontoQueue;
 
 void put(QUEUE *wholeText, char *line){
-	assert(sem_wait(&wholeText->empty) == 0);
+	//assert(sem_wait(&wholeText->empty) == 0);
 	assert(pthread_mutex_lock(&wholeText->queue_lock) == 0);
 	
 	wholeText->bufferLine[wholeText->fill] = (char*) line;
-	wholeText->fill = (wholeText->fill + 1) % wholeText->q_len;
+	wholeText->fill = (wholeText->fill + 1) % (wholeText->q_len + 1);
 	
 	assert(pthread_mutex_unlock(&wholeText->queue_lock) == 0);
-	assert(sem_post(&wholeText->full) == 0);
+	//assert(sem_post(&wholeText->full) == 0);
 }
 
 char* get(QUEUE *wholeText) {
-	assert(sem_wait(&wholeText->full) == 0);
+	//assert(sem_wait(&wholeText->full) == 0);
 	assert(pthread_mutex_lock(&wholeText->queue_lock) == 0);
-
 	char *temp = wholeText->bufferLine[wholeText->use];
-	wholeText->use = (wholeText->use + 1) % wholeText->q_len;
+	wholeText->use = (wholeText->use + 1) % (wholeText->q_len + 1);
 
 	assert(pthread_mutex_unlock(&wholeText->queue_lock) == 0);
-	assert(sem_post(&wholeText->empty) == 0);
+	//assert(sem_post(&wholeText->empty) == 0);
 
 	return temp;
+}
+
+
+//the THREAD FUNCTION
+
+void *wordCount(void *taskNum) {
+	int *taskNumInt = (int *) taskNum;
+	printf("\nThis is the taskNum = %d\n", (*taskNumInt));
+	
+	pthread_exit(NULL);
+
 }
 
 int main(int argc, char **arg) {
@@ -57,18 +67,18 @@ int main(int argc, char **arg) {
 	char *thing = "the meme\n\0";
 	char *secthing = "anothermeme\n\0";	
 	//this is the queue being intialized
-	char *cbuffer[8];
-	QUEUE q = {0, 0, 8, cbuffer, PTHREAD_MUTEX_INITIALIZER};
-	assert(sem_init(&q.empty, 0, 8) == 0);
-	assert(sem_init(&q.full, 0, 0) == 0);
+	//char *cbuffer[8];
+	//QUEUE q = {0, 0, 8, cbuffer, PTHREAD_MUTEX_INITIALIZER};
+	//assert(sem_init(&q.empty, 0, 8) == 0);
+	//assert(sem_init(&q.full, 0, 0) == 0);
 	//first round testing
-	put(&q, thing);
-	put(&q, thing);
-	char *an = get(&q);
-	char *gn = get(&q);
+	//put(&q, thing);
+	//put(&q, thing);
+	//char *an = get(&q);
+	//char *gn = get(&q);
 	
-	printf("\nThis is the meme: %s\n", thing);
-	printf("\nThis is the sec meme: %s\n",secthing);
+	//printf("\nThis is the meme: %s\n", thing);
+	//printf("\nThis is the sec meme: %s\n",secthing);
 
 	int lineCount = 0;
 	char *text = calloc(1,1), buffer[BUFFERSIZE];
@@ -89,6 +99,12 @@ int main(int argc, char **arg) {
 	strcat(text, "\0");
 	printf("\ntext:\n%s", text);
 	
+	char *cBuffer[lineCount];
+        QUEUE q = { 0, 0, lineCount, cBuffer, PTHREAD_MUTEX_INITIALIZER};
+        //assert(sem_init(&q.empty, 0, lineCount) == 0);
+        //assert(sem_init(&q.full, 0, 0) == 0);
+
+
 	//now turning the text string into lines by delimiting with \n then adding on a null
 	char *lineOnQueue = malloc(sizeof(BUFFERSIZE));
 	lineOnQueue = strtok(text, "\n");
@@ -101,24 +117,39 @@ int main(int argc, char **arg) {
 		strcpy(goingOn, lineOnQueue);
 		printf("this is goingOn = %s\n", goingOn);
 		put(&q, goingOn);
-		//char *pleaseGod = get(&q);
-		//printf("please god be a line:%s\n", pleaseGod);
 	
 	}
-
+	
+	//testing getting from the queue
 	for (int i = 0; i < lineCount - 2; i++) {
+
 
 		char *pleaseGod = get(&q);
 		printf("please god be a line: %s\n", pleaseGod);
 	}
 
-
-	//this will be the stdin
-	//FILE *input;
-	//char *fileName = arg[2];
 	int tasksToRun = atoi(arg[1]);
 	
-	printf("%d sd\n", tasksToRun);
+	pthread_t threadID[tasksToRun];
+	
+	int taskNum[tasksToRun];
+	for (int i = 0; i < tasksToRun; i++){
+		taskNum[i] = i;
+	}
+	for (int i = 0; i < tasksToRun; i++){ 
+		
+		int errorCheck = pthread_create(&threadID[i], NULL, &wordCount, (void *) taskNum[i]);
+		if (errorCheck) {
+			printf("Failed to create Thread %d", i);
+			exit(1);	
+		}
+		
+	}
+
+	
+	for (int i = 0; i < tasksToRun; i++){
+		pthread_join(threadID[i], NULL);
+	}
 
 	return 0;
 }
